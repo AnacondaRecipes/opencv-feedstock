0542b61f509f7edb47f8e0efb65b69dd164be964
Serhii Kupriienko <skupriienko@anaconda.com>
2024-06-12 14:51:34 +0300

    opencv 4.10.0 on win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,6 @@
 @echo ON
-setlocal EnableDelayedExpansion
+setlocal enabledelayedexpansion
 
-:: cmd
-echo "Building %PKG_NAME%."
-
-md build
-pushd build
 
 if "%PY3K%" == "0" (
     echo "Copying stdint.h for windows"
@@ -14,125 +9,113 @@ if "%PY3K%" == "0" (
     copy "%LIBRARY_INC%\stdint.h" %SRC_DIR%\modules\highgui\include\stdint.h
 )
 
-if "%build_variant%" == "normal" (
-  echo "Building normal variant"
-  set WITH_QT="-DWITH_QT=5"
+mkdir build
+cd build
+
+if "%qt_version%"=="5" set WITH_QT="-DWITH_QT=5"
+if "%qt_version%"=="6" set WITH_QT="-DWITH_QT=6"
+if "%qt_version%"=="none" set WITH_QT="-DWITH_QT=0"
+
+for /F "tokens=1,2 delims=. " %%a in ("%PY_VER%") do (
+   set "PY_MAJOR=%%a"
+   set "PY_MINOR=%%b"
 )
-else (
-  echo "Building headless variant"
-  set WITH_QT=0
-)
+set PY_LIB=python%PY_MAJOR%%PY_MINOR%.lib
 
 :: Workaround for building LAPACK headers with C++17
 :: see https://github.com/conda-forge/opencv-feedstock/pull/363#issuecomment-1604972688
 set "CXXFLAGS=%CXXFLAGS% -D_CRT_USE_C_COMPLEX_H"
 
+:: CMake/OpenCV like Unix-style paths for some reason.
+set UNIX_PREFIX=%PREFIX:\=/%
+set UNIX_LIBRARY_PREFIX=%LIBRARY_PREFIX:\=/%
+set UNIX_LIBRARY_BIN=%LIBRARY_BIN:\=/%
+set UNIX_LIBRARY_INC=%LIBRARY_INC:\=/%
+set UNIX_LIBRARY_LIB=%LIBRARY_LIB:\=/%
+set UNIX_SP_DIR=%SP_DIR:\=/%
+set UNIX_SRC_DIR=%SRC_DIR:\=/%
+
 :: FFMPEG building requires pkgconfig
-set PKG_CONFIG_PATH="%LIBRARY_BIN%\pkgconfig;%LIBRARY_LIB%\pkgconfig;%LIBRARY_PREFIX%\share\pkgconfig;%BUILD_PREFIX%\Library\lib\pkgconfig;%BUILD_PREFIX%\Library\bin\pkgconfig"
-set PKG_CONFIG_EXECUTABLE=%LIBRARY_BIN%\pkg-config
+set PKG_CONFIG_PATH=%UNIX_LIBRARY_PREFIX%/lib/pkgconfig
 
 
-:: The following options are set in a way to make sure that the cmake files for
-:: both the main library and modules/functionality are detected correctly by
-:: downstreams... modify with caution:  OPENCV_CONFIG_INSTALL_PATH
-:: OPENCV_INSTALL_BINARIES_PREFIX OPENCV_INSTALL_BINARIES_SUFFIX
-rem Note that though a dependency may be installed it may not be detected
-rem correctly by this build system and so some functionality may be disabled
-rem (this is more frequent on Windows but does sometimes happen on other OSes).
-rem Note that -DBUILD_x=0 may not be honoured for any particular dependency x.
-rem If -DHAVE_x=1 is used it may be that the undetected conda package is
-rem ignored in lieu of libraries that are built as part of this build (this
-rem will likely result in an overdepending error). Check the 3rdparty libraries
-rem directory in the build directory to see what has been vendored by the
-rem opencv build
-
-:: Generate the build files.
-echo "Generating the build files..."
-cmake -L .. %CMAKE_ARGS% -G Ninja             ^
-    -DCMAKE_CXX_STANDARD=17                  ^
-    -DCMAKE_BUILD_TYPE="Release"   ^
-    -DCMAKE_FIND_ROOT_PATH=%PREFIX%;%BUILD_PREFIX%  ^
-    -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%  ^
-    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%         ^
-    -DCMAKE_SYSTEM_PREFIX_PATH=%LIBRARY_PREFIX%     ^
-    -DOPENCV_CONFIG_INSTALL_PATH="cmake"       ^
-    -DOPENCV_BIN_INSTALL_PATH=bin                   ^
-    -DOPENCV_LIB_INSTALL_PATH=lib                   ^
-    -DOPENCV_DOWNLOAD_PARAMS=INACTIVITY_TIMEOUT;30;TIMEOUT;180;SHOW_PROGRESS  ^
-    -DOPENCV_DOWNLOAD_TRIES=1;2;3;4;5               ^
-    -DENABLE_CONFIG_VERIFICATION=ON                 ^
-    -DOPENCV_GENERATE_PKGCONFIG=ON ^
-    -DBUILD_DOCS=0                 ^
-    -DBUILD_IPP_IW=0               ^
-    -DBUILD_JASPER=0               ^
-    -DBUILD_JAVA=0                 ^
-    -DBUILD_JPEG=0                 ^
-    -DBUILD_OPENEXR=0              ^
-    -DBUILD_OPENJPEG=0             ^
-    -DBUILD_PERF_TESTS=0           ^
-    -DBUILD_PNG=0                  ^
-    -DBUILD_PROTOBUF=0             ^
-    -DBUILD_SHARED_LIBS=ON         ^
-    -DBUILD_TBB=0                  ^
-    -DBUILD_TESTS=0                ^
-    -DBUILD_TIFF=0                 ^
-    -DBUILD_WEBP=0                 ^
-    -DBUILD_WITH_STATIC_CRT=0      ^
-    -DBUILD_ZLIB=0                 ^
-    -DBUILD_opencv_bioinspired=0   ^
-    -DBUILD_opencv_python2=0       ^
-    -DBUILD_opencv_python3=1       ^
-    -DENABLE_PRECOMPILED_HEADERS=OFF                ^
-    -DINSTALL_C_EXAMPLES=0                          ^
-    -DOPENCV_EXTRA_MODULES_PATH=%SRC_DIR%\opencv_contrib-%PKG_VERSION%\modules      ^
-    -DOPENCV_GENERATE_SETUPVARS=OFF                 ^
+cmake -LAH -G "Ninja"                                                               ^
+    -DCMAKE_CXX_STANDARD=17                                                         ^
+    -DCMAKE_BUILD_TYPE="Release"                                                    ^
+    -DCMAKE_INSTALL_PREFIX=%UNIX_LIBRARY_PREFIX%                                    ^
+    -DCMAKE_PREFIX_PATH=%UNIX_LIBRARY_PREFIX%                                       ^
+    -DOPENCV_CONFIG_INSTALL_PATH=cmake                                              ^
+    -DOPENCV_BIN_INSTALL_PATH=bin                                                   ^
+    -DOPENCV_LIB_INSTALL_PATH=lib                                                   ^
+    -DOPENCV_GENERATE_SETUPVARS=OFF                                                 ^
+    -DOPENCV_DOWNLOAD_TRIES=1;2;3;4;5                                               ^
+    -DOPENCV_DOWNLOAD_PARAMS=INACTIVITY_TIMEOUT;30;TIMEOUT;180;SHOW_PROGRESS        ^
+    -DWITH_LAPACK=0                                                                 ^
+    -DWITH_EIGEN=1                                                                  ^
+    -DENABLE_CONFIG_VERIFICATION=ON                                                 ^
+    -DENABLE_PRECOMPILED_HEADERS=OFF                                                ^
+    -DBUILD_TESTS=0                                                                 ^
+    -DBUILD_DOCS=0                                                                  ^
+    -DBUILD_PERF_TESTS=0                                                            ^
+    -DBUILD_ZLIB=0                                                                  ^
+    -DBUILD_opencv_bioinspired=0                                                    ^
+    -DBUILD_TIFF=0                                                                  ^
+    -DBUILD_PNG=0                                                                   ^
+    -DWITH_PROTOBUF=1                                                               ^
+    -DBUILD_PROTOBUF=0                                                              ^
+    -DPROTOBUF_UPDATE_FILES=1                                                       ^
+    -DBUILD_OPENEXR=0                                                               ^
+    -DWITH_OPENEXR=1                                                                ^
+    -DBUILD_JASPER=0                                                                ^
+    -DWITH_JASPER=1                                                                 ^
+    -DWITH_OPENJPEG=0                                                               ^
+    -DBUILD_JPEG=0                                                                  ^
+    -DWITH_CUDA=0                                                                   ^
+    -DWITH_OPENCL=0                                                                 ^
+    -DWITH_OPENCLAMDFFT=0                                                           ^
+    -DWITH_OPENCLAMDBLAS=0                                                          ^
+    -DWITH_OPENCL_D3D11_NV=0                                                        ^
+    -DWITH_OPENVINO=0                                                               ^
+    -DWITH_1394=0                                                                   ^
+    -DWITH_OPENNI=0                                                                 ^
+    -DWITH_HDF5=1                                                                   ^
     -DOPENCV_ENABLE_PKG_CONFIG=1                                                    ^
-    -DOPENCV_PYTHON2_INSTALL_PATH=""                ^
-    -DPYTHON3_PACKAGES_PATH=%SP_DIR%          ^
-    -DOPENCV_PYTHON3_INSTALL_PATH=%SP_DIR%          ^
-    -DOPENCV_SKIP_PYTHON_LOADER=1                   ^
+    -DWITH_FFMPEG=0                                                                 ^
+    -DWITH_TENGINE=0                                                                ^
+    -DWITH_GSTREAMER=0                                                              ^
+    -DWITH_TESSERACT=0                                                              ^
+    -DWITH_VTK=0                                                                    ^
+    -DWITH_WIN32UI=0                                                                ^
+    -DINSTALL_C_EXAMPLES=0                                                          ^
+    -DOPENCV_EXTRA_MODULES_PATH=%UNIX_SRC_DIR%/opencv_contrib/modules               ^
+    -DPYTHON_EXECUTABLE=""                                                          ^
+    -DPYTHON_INCLUDE_DIR=""                                                         ^
+    -DPYTHON_PACKAGES_PATH=""                                                       ^
+    -DPYTHON_LIBRARY=""                                                             ^
+    -DPYTHON_NUMPY_INCLUDE_DIRS=""                                                  ^
+    -DBUILD_opencv_python2=0                                                        ^
+    -DPYTHON2_EXECUTABLE=""                                                         ^
+    -DPYTHON2_INCLUDE_DIR=""                                                        ^
+    -DPYTHON2_NUMPY_INCLUDE_DIRS=""                                                 ^
+    -DPYTHON2_LIBRARY=""                                                            ^
+    -DPYTHON2_PACKAGES_PATH=""                                                      ^
+    -DOPENCV_PYTHON2_INSTALL_PATH=""                                                ^
+    -DBUILD_opencv_python3=0                                                        ^
+    -DPYTHON_EXECUTABLE=%UNIX_PREFIX%/python                                        ^
+    -DPYTHON_INCLUDE_DIR=%UNIX_PREFIX%/include                                      ^
+    -DPYTHON_PACKAGES_PATH=%UNIX_SP_DIR%                                            ^
+    -DPYTHON_LIBRARY=%UNIX_PREFIX%/libs/%PY_LIB%                                    ^
+    -DBUILD_opencv_python3=1                                                        ^
+    -DOPENCV_SKIP_PYTHON_LOADER=1                                                   ^
+    -DPYTHON3_EXECUTABLE=%UNIX_PREFIX%/python                                       ^
+    -DPYTHON3_INCLUDE_DIR=%UNIX_PREFIX%/include                                     ^
+    -DPYTHON3_LIBRARY=%UNIX_PREFIX%/libs/%PY_LIB%                                   ^
+    -DPYTHON3_PACKAGES_PATH=%UNIX_SP_DIR%                                           ^
+    -DOPENCV_PYTHON3_INSTALL_PATH=%UNIX_SP_DIR%                                     ^
     -DOPENCV_PYTHON_PIP_METADATA_INSTALL=ON                                         ^
     -DOPENCV_PYTHON_PIP_METADATA_INSTALLER:STRING="conda"                           ^
-    -DPROTOBUF_UPDATE_FILES=1                       ^
-    -DPYTHON3_EXECUTABLE=%PREFIX%\python.exe        ^
-    -DPYTHON_DEFAULT_EXECUTABLE=%PREFIX%\python.exe ^
-    -DWEBP_LIBRARY=%LIBRARY_LIB%\libwebp.lib ^
-    -DWEBP_INCLUDE_DIR=%LIBRARY_INC%     ^
-    -DWITH_1394=0                                   ^
-    -DWITH_CUDA=0                                   ^
-    -DWITH_DIRECTX=0                                ^
-    -DWITH_EIGEN=1                                  ^
-    -DWITH_FFMPEG=0                                 ^
-    -DWITH_FREETYPE=1                               ^
-    -DWITH_GSTREAMER=1                              ^
-    -DWITH_GTK=0                                    ^
-    -DWITH_HDF5=1                                   ^
-    -DWITH_JASPER=1                                 ^
-    -DWITH_LAPACK=0                                 ^
-    -DWITH_MSMF_DXVA=0                              ^
-    -DWITH_OPENCL=0                                 ^
-    -DWITH_OPENCLAMDBLAS=0                          ^
-    -DWITH_OPENCLAMDFFT=0                           ^
-    -DWITH_OPENCL_D3D11_NV=0                        ^
-    -DWITH_OPENJPEG=0                               ^
-    -DWITH_OPENNI=0                                 ^
-    -DWITH_PROTOBUF=1                               ^
-    %WITH_QT%                                       ^
-    -DWITH_ZLIB=1 -DWITH_PNG=1 -DWITH_TIFF=1 -DWITH_TBB=0      ^
-    -DWITH_TENGINE=0                                ^
-    -DWITH_TESSERACT=0                              ^
-    -DWITH_VTK=0                                    ^
-    -DWITH_WEBP=1                                   ^
-    -DWITH_WIN32UI=0
-
+    ..
 if %ERRORLEVEL% neq 0 (type CMakeError.log && exit 1)
 
-cmake --build . -j%CPU_COUNT%
+cmake --build . --target install --config Release
 if %ERRORLEVEL% neq 0 exit 1
-
-echo cmake --build . ok
-echo exiting the lot OK.
-
-:: Error free exit.
-echo "Error free exit!"
-exit /b 0
